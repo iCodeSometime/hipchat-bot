4b83db771733a4306baa0107689d211276e95129
Kenneth Cochran <kenneth.cochran101@gmail.com>
2018-12-13 10:42:51 -0600

    db_provide should return a promise to be more flexible

--- shared/db_provider.js
+++ shared/db_provider.js
@@ -7,19 +7,13 @@ use_ssl = true;
 // Easier than figuring out why psql won't allow ssl locally
 if (process.env.dev_env) use_ssl = false;
 
-function run_sql(query) {
+async function run_sql(query) {
   const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: use_ssl
   });
 
   client.connect();
-
-  client.query(query, (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  });
+  ret = undefined;
+  return client.query(query);
 }
